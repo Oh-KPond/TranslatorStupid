5f9fbeff8050a392f9c654fba062e17ca4688de9
Shubha Rajan <shubharajan94@gmail.com>
2019-08-27 22:20:08 -0700

    Wrapped JS in DOMContentLoaded event listener

--- translator.js
+++ translator.js
@@ -1,18 +1,20 @@
-document.getElementById("translate-btn").addEventListener("click", function(){
-  const phrases = [
-    'Avoir un chat dans la gorge',
-    'Ah, la vache!',
-    'Devenir chêvre',
-    'Faire l’andouille',
-    'Tremper son biscuit'
-  ]
-  // To have a cat in the throat
-  // Oh, my cow
-  // To become a goat
-  // To make the sausage
-  // Dip their biscuit
+document.addEventListener("DOMContentLoaded", function(){
+  document.getElementById("translate-btn").addEventListener("click", function(){
+    const phrases = [
+      'Avoir un chat dans la gorge',
+      'Ah, la vache!',
+      'Devenir chêvre',
+      'Faire l’andouille',
+      'Tremper son biscuit'
+    ]
+    // To have a cat in the throat
+    // Oh, my cow
+    // To become a goat
+    // To make the sausage
+    // Dip their biscuit
 
-  console.log(phrases)
+    console.log(phrases)
 
-  return alert(phrases[Math.floor(Math.random()*phrases.length)])
+    return alert(phrases[Math.floor(Math.random()*phrases.length)])
+  });
 });
